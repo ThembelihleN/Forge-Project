f0f133d2ba6360dc85bd0e561ce92530944e40b8
ThembelihleN <thembelihlengobese3@gmail.com>
2024-06-24 13:25:34 +0200

    added search  variables

--- responsive-book-website-main/responsive-book-website-main/assets/js/main.js
+++ responsive-book-website-main/responsive-book-website-main/assets/js/main.js
@@ -1,6 +1,23 @@
 /*=============== SEARCH ===============*/
-const navMenu = document.getElementById('nav-menu'),
-navToggle = document.getElementById('nav-toggle')
+const searchButton = document.getElementById('search-button'),
+searchClose = document.getElementById('search-close'),
+searchContent = document.getElementById('search-content')
+
+/*=============== MENU SHOW ========== */
+/* Validate if constant exists */
+if(navtoggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*======= MENU HIDDEN ==========*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList('show-menu')
+    })
+}
 
 
 
