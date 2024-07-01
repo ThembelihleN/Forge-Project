f44cca6e0df4f33134b52a2a896bb5b761d25b6d
ThembelihleN <thembelihlengobese3@gmail.com>
2024-07-01 08:39:30 +0200

    deleted a test file

--- responsive-book-website-main/responsive-book-website-main/assets/js/main.js
+++ responsive-book-website-main/responsive-book-website-main/assets/js/main.js
@@ -1,65 +1,64 @@
 /*=============== SEARCH ===============*/
 const searchButton = document.getElementById('search-button'),
-searchClose = document.getElementById('search-close'),
-searchContent = document.getElementById('search-content')
+    searchClose = document.getElementById('search-close'),
+    searchContent = document.getElementById('search-content');
 
 /*=============== SEARCH SHOW ========== */
 /* Validate if constant exists */
-if(searchButton){
-    searchButton.addEventListener('click', () =>{
-        searchContent.classList.add('show-search')
-    })
+if (searchButton) {
+    searchButton.addEventListener('click', () => {
+        searchContent.classList.add('show-search');
+    });
 }
 
 /*======= SEARCH HIDDEN ==========*/
 /* Validate if constant exists */
-if(searchClose){
-    searchClose.addEventListener('click', () =>{
-        searchContent.classList.remove('show-search')
-    })
+if (searchClose) {
+    searchClose.addEventListener('click', () => {
+        searchContent.classList.remove('show-search');
+    });
 }
 
 /*=============== LOGIN ===============*/
 const loginButton = document.getElementById('login-button'),
-loginClose = document.getElementById('login-close'),
-loginContent = document.getElementById('login-content')
+    loginClose = document.getElementById('login-close'),
+    loginContent = document.getElementById('login-content');
 
 /*=============== LOGIN SHOW ============== */
 /* Validate if constant exists */
-if(loginButton){
-    loginButton.addEventListener('click', () =>{
-        loginContent.classList.add('show-login')
-    })  
+if (loginButton) {
+    loginButton.addEventListener('click', () => {
+        loginContent.classList.add('show-login');
+    });
 }
 
 /*============== LOGIN HIDDEN ================ */
 /* Validate if constant exists */
-if(loginClose){
-    loginClose.addEventListener('click', () =>{
-        loginContent.classList.remove('show-login')
-    })
+if (loginClose) {
+    loginClose.addEventListener('click', () => {
+        loginContent.classList.remove('show-login');
+    });
 }
 
 /*=============== ADD SHADOW HEADER ===============*/
 const shadowHeader = () => {
-    const header = getElementById('header')
+    const header = document.getElementById('header');
     //when the scroll is greater than 50 viewport height,
-    this.scrollY >= 50 ? header.classList.add('shadow-header')
-                        :header.classList.remove('shadow-header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header');
 }
-window.addEventListener('scroll', shadowHeader)
+window.addEventListener('scroll', shadowHeader);
 
 /*=============== HOME SWIPER ===============*/
-let swiperHome = new swiper('.home__swiper', {
+let swiperHome = new Swiper('.home__swiper', {
     loop: true,
     spaceBetween: -24,
     grabCursor: true,
     slidesPerView: 'auto',
-    centeredSlides: 'auto',
+    centeredSlides: auto,
 
     autoplay: {
         delay: 3000,
-        dissableOnInteraction: false,
+        disableOnInteraction: false,
     },
 
     breakpoints: {
@@ -67,25 +66,18 @@ let swiperHome = new swiper('.home__swiper', {
             spaceBetween: -32,
         }
     }
-})
-
+});
 
 /*=============== FEATURED SWIPER ===============*/
 
-
 /*=============== NEW SWIPER ===============*/
-
 
 /*=============== TESTIMONIAL SWIPER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
